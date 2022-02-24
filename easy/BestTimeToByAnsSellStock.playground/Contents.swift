1b8f80b832b24f3c662dd8ebc947c7fe3243a2d0
Stepan Kirillov <stepakirillov59@gmail.com>
2022-02-24 18:29:52 +0300

    Norm

--- easy/BestTimeToByAnsSellStock.playground/Contents.swift
+++ easy/BestTimeToByAnsSellStock.playground/Contents.swift
@@ -1,16 +1,22 @@
 import UIKit
 
+
+//check it tomorow
 func maxProfit(_ prices: [Int]) -> Int {
 	if (prices.count < 2) {
 		return 0
 	}
-	var max = 0
+	var smallest = Int.max
+	var maxProfit = 0
+	var now = 0
 	for i in 0..<prices.count {
-		for j in i + 1..<prices.count {
-			if (max < prices[j] - prices[i]) {
-				max = prices[j] - prices[i]
-			}
+		if (prices[i] < smallest) {
+			smallest = prices[i]
+		}
+		now = prices[i] - smallest
+		if (maxProfit < now) {
+			maxProfit = now
 		}
 	}
-	return (max)
+	return (maxProfit)
 }
